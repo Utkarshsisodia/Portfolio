1212bf3262075562e70802710ab99b7f5e2da4a5
Utkarshsisodia <usisodia67@gmail.com>
2025-06-30 18:11:53 +0530

    Initial files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 class=" font-sans text-5xl antialiased font-bold text-center text-gray-800  mt-8">This is a heading</h1> 
+      <p class="font-sans text-2xl antialiased font-light text-center text-gray-600 tracking-wider mt-20">This is my Portfolio webpage.</p>
+        
+    </>
+  )
+}
+
+export default App
