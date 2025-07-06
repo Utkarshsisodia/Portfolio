2c0c9b4471792e76e249169dca19c7e2d0d8c0d8
utkarsh <usisodia67@gmail.com>
2025-07-06 19:56:15 +0530

    Implement Delete Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,54 @@
-
-import './App.css'
+import { useState } from "react";
 
 function App() {
+  let[item, setItem] = useState('');
+  let[itemList, setItemList] = useState([]);
 
+  let handleData = (e) => {
+    let itemValue = e.target.value ;
+    setItem(itemValue);    
+  }
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    setItemList([...itemList, item])
+  }
+
+  let handleDeleteItem = (id) => {
+    
+    let newList = itemList.filter((el, i) => itemList.indexOf(el) !== id);
+    setItemList([...newList]);
+    console.log(newList);
+  }
 
   return (
-    <>
-      <h1 class=" font-sans text-5xl antialiased font-bold text-center text-gray-800  mt-8">This is a heading</h1> 
-      <p class="font-sans text-2xl antialiased font-light text-center text-gray-600 tracking-wider mt-20">This is my Portfolio webpage.</p>
-        
-    </>
-  )
+    <div className="flex justify-center items-center flex-col gap-16">
+      <h1 className="flex gap-8 text-5xl text-center antialiased font-bold text-gray-800  mt-20">
+        ToDo
+        <span>
+          <img src="src\assets\pencil.svg" width="40px"></img>
+        </span>
+      </h1>
+      <div className="input-form-container h-24 flex items-center">
+        <form onSubmit={handleSubmit} >
+          <input
+            required
+            onChange={handleData}
+            type="text"
+            name="todo"
+            id="todo"
+            placeholder="Enter task todo.."
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div className="list-container">
+        <ul id="list">
+          {itemList.map((it, i) => <li key={i} id={i} className="list-item" onClick={()=> handleDeleteItem(i)} >{it}</li>)}
+        </ul> 
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
